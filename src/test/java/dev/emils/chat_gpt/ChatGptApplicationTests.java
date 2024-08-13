b543d602140941b383c9efb1046e061514962796
Emils <135007928+emilsViksnins@users.noreply.github.com>
2024-08-13 14:13:17 +0300

    first commit

---
+++ src/test/java/dev/emils/chat_gpt/ChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emils.chat_gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
